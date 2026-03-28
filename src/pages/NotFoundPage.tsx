6d37903371c7916ddd6b50004f523ce8086e8eba
prashant4840 <prashantsharma4840@icloud.com>
2026-03-28 21:41:43 +0530

    404 page

---
+++ src/pages/NotFoundPage.tsx
@@ -0,0 +1,21 @@
+import { ButtonLink } from '@/components/common/ButtonLink'
+import { EmptyState } from '@/components/common/EmptyState'
+import { PageTransition } from '@/components/common/PageTransition'
+
+export function NotFoundPage() {
+  return (
+    <PageTransition>
+      <div className="page-shell">
+        <EmptyState
+          title="Page not found"
+          description="The route you tried to open does not exist in this SkillBridge build."
+          action={
+            <ButtonLink to="/" variant="outline">
+              Back home
+            </ButtonLink>
+          }
+        />
+      </div>
+    </PageTransition>
+  )
+}
